e29dfee4b05edcb5c1f796962af4e6a042bdfdf6
Virginia <vcooper_us@yahoo.com>
2015-08-12 00:49:02 -0700

    Second attempt at correct configuration for Production emails.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,4 +80,9 @@ Rails.application.configure do
 
   # http://bloccit.herokuapp.com
   config.action_mailer.default_url_options = { host: 'vrcooper-bloccit.herokuapp.com'}
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true 
+
+  config.action_mailer.raise_delivery_errors = true
 end
