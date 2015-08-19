a6665f7c4c3f1938c595f9a90f0fe186ed72775a
Virginia <vcooper_us@yahoo.com>
2015-08-18 21:31:38 -0700

    Successfully uploaded image.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,10 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
-         has_many :posts
+         
+  has_many :posts
+
+  mount_uploader :avatar, AvatarUploader
 
   def admin?
     role == 'admin'
