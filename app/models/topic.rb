f14ee45ac187afe28c39eaf9fc055b0e377749d8
Virginia <vcooper_us@yahoo.com>
2015-08-19 01:40:38 -0700

    Completed pagination assignment.

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,8 +1,10 @@
 class Topic < ActiveRecord::Base
   has_many :posts
-
+  self.per_page = 50
   
   
   validates :name, length: { minimum: 5 }, presence: true
   
+  
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -21,7 +21,7 @@ users = User.all
 # The 'save' method then saves this User to the database.
 
 # Create Topics
-15.times do
+500.times do
   Topic.create!(
     name:  Faker::Lorem.sentence,
     description: Faker::Lorem.paragraph
@@ -30,7 +30,7 @@ end
 topics = Topic.all
 
 #Create Posts
-50.times do 
+100000.times do 
   Post.create!(
     user:  users.sample,
     topic: topics.sample,
