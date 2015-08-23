9ffabd48baaf8f28f43573d546b54f54df787dee
Virginia <vcooper_us@yahoo.com>
2015-08-22 21:02:13 -0700

    Updated posts_controller.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -20,7 +20,7 @@ class PostsController < ApplicationController
     @post.user = current_user
     @post.topic =  @topic
     authorize @post
-    authorize @comment
+    #authorize @comment
     
     if @post.save
       flash[:notice] = "Post was saved."
