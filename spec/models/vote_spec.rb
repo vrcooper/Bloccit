c2552117528dbb592c7a875a96af9843ed7297fd
Virginia <vcooper_us@yahoo.com>
2015-08-25 01:08:06 -0700

    Created vote_spec.rb.

---
+++ spec/models/vote_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+describe Vote  do
+  describe "validations" do
+    describe "value validation" do
+      it "only allows -1 or 1 as values" do
+        # your expectations here
+        #expect(vote.value).to eq(-1 || 1)
+
+        up_vote = Vote.new(value: 1)
+        down_vote = Vote.new(value: -1)
+        nil_vote = Vote.new(value: 0)
+
+        expect(up_vote.valid?).to eq(true)
+        expect(down_vote.valid?).to eq(true)
+        expect(nil_vote.valid?).to eq(false)
+      end
+    end 
+  end  
+end
