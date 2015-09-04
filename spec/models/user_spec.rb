4238104d1bd7dc6cd83018f8f6d001fc5daaa54e
Virginia <vcooper_us@yahoo.com>
2015-09-03 23:41:39 -0700

    unsuccessful user_spec.rb test.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,10 +5,25 @@ describe User  do
   include TestFactories
 
   describe "#favorited(post)" do
-    xit "returns 'nil' if the user has not favorited the post" do
+
+    before do
+      @user = create(:user)
+      @post = create(:post, user: @user)
+      @another_post = create(:post, user: @user)
+      end
+
+    it "returns 'nil' if the user has not favorited the post" do
+      expect(@user.favorites.find_by_post_id(@post.id) ).to be nil
     end
 
-    xit "returns the appropriate favorite if it exists" do
+    it "returns the appropriate favorite if it exists" do
+      favorite = @user.favorites.create!(post: @post)
+      expect(@user.favorites.find_by_post_id(@post.id) ).to eq(favorite)
+    end
+
+    it "returns 'nil' if the user has favorited another post" do
+      @user.favorites.create!(post: @another_post)
+      expect(@user.favorites.find_by_post_id(@post.id) ).to be nil
     end
   end
 end
