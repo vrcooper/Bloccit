b34bd4881371443215829ab9a7adeecc403fc180
Virginia <vcooper_us@yahoo.com>
2015-09-08 19:08:02 -0700

    Successfully implemented assignment 54.

--- spec/features/profiles_spec.rb
+++ spec/features/profiles_spec.rb
@@ -1,9 +1,13 @@
 require 'rails_helper'
 
+ include TestFactories
+
+  include Warden::Test::Helpers
+  Warden.test_mode!
+
+
 describe "Visiting profiles" do 
-
-  include TestFactories
-
+  
   before do
     @user = authenticated_user
     @post = associated_post(user: @user)
@@ -25,4 +29,21 @@ describe "Visiting profiles" do
     end
   end
   
+  describe "signed in viewing own profile" do
+
+    before do
+      #user = authenticated_user
+      login_as(@user, :scope => :user)
+    end
+
+    it "shows profile" do
+      visit user_path(@user)
+      expect(current_path).to eq(user_path(@user))
+
+      expect( page ).to have_content(@user.name)
+      expect( page ).to have_content(@post.title)
+      expect( page ).to have_content(@comment.body)
+    end
+  end
+
 end
