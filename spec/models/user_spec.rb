3b687c77c804bbab3839a4b4387529b59ba94b45
Virginia <vcooper_us@yahoo.com>
2015-09-09 14:27:56 -0700

    checkpoint 51 completed

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -13,17 +13,20 @@ describe User  do
       end
 
     it "returns 'nil' if the user has not favorited the post" do
-      expect(@user.favorites.find_by_post_id(@post.id) ).to be nil
+      expect(@user.favorited(@post)).to be nil 
+      #expect(@user.favorites.find_by_post_id(@post.id) ).to be nil
     end
 
     it "returns the appropriate favorite if it exists" do
       favorite = @user.favorites.create!(post: @post)
-      expect(@user.favorites.find_by_post_id(@post.id) ).to eq(favorite)
+      expect(@user.favorited(@post)).to eq(favorite) 
+      #expect(@user.favorites.find_by_post_id(@post.id) ).to eq(favorite)
     end
 
     it "returns 'nil' if the user has favorited another post" do
       @user.favorites.create!(post: @another_post)
-      expect(@user.favorites.find_by_post_id(@post.id) ).to be nil
+      expect(@user.favorited(@post)).to be nil
+      #expect(@user.favorites.find_by_post_id(@post.id) ).to be nil
     end
   end
 end
